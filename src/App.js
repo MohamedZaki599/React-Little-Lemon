fa2a173b2e45022c568538127457b22f6ffe1a3c
MohamedZaki599 <59538937+MohamedZaki599@users.noreply.github.com>
2025-01-20 19:49:34 +0200

    Refactor component structure and update dependencies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import "./App.css"
 import React from "react"
-import Header from "./Header"
-import Nav from "./Nav"
-import Main from "./Main"
-import Footer from "./Footer"
+import Nav from "./components/Nav"
+import Main from "./components/Main"
+import Menu from "./components/Menu"
+import Footer from "./components/Footer"
 
 function App() {
 	return (
 		<>
-			<Header />
 			<Nav />
 			<Main />
+			<Menu />
 			<Footer />
 		</>
 	)
